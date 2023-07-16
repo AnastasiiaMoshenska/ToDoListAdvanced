8e69067b4856c8b46a252c2767cccf03edd0e61b
amoshens <anastasiia.moshenska@stud.hslu.ch>
2023-07-16 16:28:49 +0200

    Project initialisation

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SettingsComponent } from './settings/settings.component';
+import { TaskComponent } from './task/task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    SettingsComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
