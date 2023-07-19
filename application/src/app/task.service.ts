d7a918de154eb636306d17284ef31a39a3e36d06
amoshens <anastasiia.moshenska@stud.hslu.ch>
2023-07-19 11:37:14 +0200

    Add task component is added. Id fix is needed

--- application/src/app/task.service.ts
+++ application/src/app/task.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable, of, tap} from "rxjs";
+import {Observable, of} from "rxjs";
 import {Task} from "./task";
 import {catchError} from "rxjs/operators";
 
